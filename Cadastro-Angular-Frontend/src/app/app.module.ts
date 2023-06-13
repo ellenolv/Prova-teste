846a4774ce1503b75e6b47b8e4ecbd6785f8df75
ellenolv <ellendeoliveirapereira@gmail.com>
2023-06-13 08:35:30 -0300

    2. Criar todos os componentes, serviços e interfaces

--- Cadastro-Angular-Frontend/src/app/app.module.ts
+++ Cadastro-Angular-Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnunciosComponent } from './anuncios/anuncios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnunciosComponent
   ],
   imports: [
     BrowserModule,
